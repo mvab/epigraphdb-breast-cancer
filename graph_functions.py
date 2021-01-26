e55f01a41f3a81562aa79f91d9ec1124135260f8
Marina Vabistsevits <ny19205@bristol.ac.uk>
2021-01-26 18:25:13 +0000

    add alt server option

--- graph_functions.py
+++ graph_functions.py
@@ -18,6 +18,26 @@ def start_graph_session(env):
     return session
 
 
+def start_graph_session_crashdown(env):
+    # no auth required
+
+    from neo4j import GraphDatabase
+    env.read_env()
+
+    EPIGRAPHDB_SERVER = env.str("EPIGRAPHDB_SERVER_CRASHDOWN")
+    EPIGRAPHDB_PORT = env.str("EPIGRAPHDB_PORT_CRASHDOWN")
+    print(EPIGRAPHDB_PORT)
+
+    epigraphdb_driver = GraphDatabase.driver(
+        "bolt://{server_name}:{port}".format(
+            server_name=EPIGRAPHDB_SERVER,
+            port=EPIGRAPHDB_PORT)
+    )
+    
+    session = epigraphdb_driver.session()
+    return session
+
+
 
 def query_to_df(session, query, print_query=True):
     import pandas as pd
@@ -26,3 +46,22 @@ def query_to_df(session, query, print_query=True):
     data=session.run(query).data()
     df = pd.json_normalize(data)
     return(df)
+
+
+
+#### other functions
+
+def is_it_cancer(df, disease_column_name):
+    # check which of those diseases are cancer related
+    cancer_related = ['cancer', 'malignant', 'tumor', 'neoplasm', 'lymphoma', 'carcin', 'oma', 'leukemia', 'metasta', 'myelofibrosis']
+    df['is_it_cancer'] = 'no'
+
+    for ind in df.index:
+        label = df[disease_column_name][ind]
+        res = [ele for ele in cancer_related if(ele in label)] 
+        if bool(res):
+            df['is_it_cancer'][ind]= 'yes'
+    return df
+
+
+
