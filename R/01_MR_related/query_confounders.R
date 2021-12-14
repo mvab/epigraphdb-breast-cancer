c349cfd368565c31040f3ad821f9de3e94d5f9f1
Marina Vabistsevits <ny19205@bristol.ac.uk>
2021-12-14 13:14:24 +0000

    confounders query

--- R/01_MR_related/query_confounders.R
+++ R/01_MR_related/query_confounders.R
@@ -3,7 +3,7 @@ library(epigraphdb)
 source("helper_functions.R")
 
 
-exposure = "prot-a-1097"
+exposure = "met-a-362"
 outcome = "ieu-a-1128"
 pval_threshold = 1e-04
 
@@ -39,50 +39,52 @@ reverse_mediator_query = paste0("
     RETURN exposure {.id, .trait}, outcome {.id, .trait}, med {.id, .trait}, r1 {.b, .se, .pval, .selection, .method, .moescore}, r2 {.b, .se, .pval, .selection, .method, .moescore}, r3 {.b, .se, .pval, .selection, .method, .moescore} ORDER BY r1.p
           ")
 
-
-test<-query_epigraphdb_as_table(mediator_query)
-xx<-tidy_conf_query_output(test, type = "mediator")
-
-
-
+# testing
+#test<-query_epigraphdb_as_table(mediator_query)
+#xx<-tidy_conf_query_output(test, type = "mediator")
 
 tidy_conf_query_output <- function(df, type){
-  df_OR <- df %>% 
-  mutate( r1.loci = r1.b - 1.96 * r1.se, 
-          r1.upci = r1.b + 1.96 * r1.se,
-          r1.or = exp(r1.b), 
-          r1.or_loci = exp(r1.loci), 
-          r1.or_upci = exp(r1.upci),
-          r1.OR_CI = paste0(round(r1.or,2), " [",round(r1.or_loci,2) ,":",round(r1.or_upci,2), "]")) %>% 
-    mutate(r1.effect_direction = ifelse(r1.or_loci > 1 & r1.or_upci >= 1, 'positive',
-                                 ifelse(r1.or_loci < 1 & r1.or_upci <= 1, 'negative', 'overlaps null'))) %>% 
-    mutate( r2.loci = r2.b - 1.96 * r2.se, 
-            r2.upci = r2.b + 1.96 * r2.se,
-            r2.or = exp(r2.b), 
-            r2.or_loci = exp(r2.loci), 
-            r2.or_upci = exp(r2.upci),
-            r2.OR_CI = paste0(round(r2.or,2), " [",round(r2.or_loci,2) ,":",round(r2.or_upci,2), "]")) %>% 
-    mutate(r2.effect_direction = ifelse(r2.or_loci > 1 & r2.or_upci >= 1, 'positive',
-                                 ifelse(r2.or_loci < 1 & r2.or_upci <= 1, 'negative', 'overlaps null'))) %>% 
   
-    mutate( r3.loci = r3.b - 1.96 * r3.se, 
-            r3.upci = r3.b + 1.96 * r3.se,
-            r3.or = exp(r3.b), 
-            r3.or_loci = exp(r3.loci), 
-            r3.or_upci = exp(r3.upci),
-            r3.OR_CI = paste0(round(r3.or,2), " [",round(r3.or_loci,2) ,":",round(r3.or_upci,2), "]")) %>% 
-    mutate(r3.effect_direction = ifelse(r3.or_loci > 1 & r3.or_upci >= 1, 'positive',
-                                 ifelse(r3.or_loci < 1 & r3.or_upci <= 1, 'negative', 'overlaps null'))) %>% 
+  if (dim(df)[1] != 0){
     
-    select(-contains("loci"), -contains("upci")) %>% 
-  
-    select("exposure.trait", "exposure.id", "outcome.trait", "outcome.id", "med.trait","med.id" , 
-           starts_with("r1"),  starts_with("r2"),starts_with("r3")) %>% 
-    filter(r1.effect_direction != 'overlaps null' &r2.effect_direction != 'overlaps null' & r3.effect_direction != 'overlaps null') %>% 
-    mutate(type = type)
+    df_OR <- df %>% 
+    mutate( r1.loci = r1.b - 1.96 * r1.se, 
+            r1.upci = r1.b + 1.96 * r1.se,
+            r1.or = exp(r1.b), 
+            r1.or_loci = exp(r1.loci), 
+            r1.or_upci = exp(r1.upci),
+            r1.OR_CI = paste0(round(r1.or,2), " [",round(r1.or_loci,2) ,":",round(r1.or_upci,2), "]")) %>% 
+      mutate(r1.effect_direction = ifelse(r1.or_loci > 1 & r1.or_upci >= 1, 'positive',
+                                   ifelse(r1.or_loci < 1 & r1.or_upci <= 1, 'negative', 'overlaps null'))) %>% 
+      mutate( r2.loci = r2.b - 1.96 * r2.se, 
+              r2.upci = r2.b + 1.96 * r2.se,
+              r2.or = exp(r2.b), 
+              r2.or_loci = exp(r2.loci), 
+              r2.or_upci = exp(r2.upci),
+              r2.OR_CI = paste0(round(r2.or,2), " [",round(r2.or_loci,2) ,":",round(r2.or_upci,2), "]")) %>% 
+      mutate(r2.effect_direction = ifelse(r2.or_loci > 1 & r2.or_upci >= 1, 'positive',
+                                   ifelse(r2.or_loci < 1 & r2.or_upci <= 1, 'negative', 'overlaps null'))) %>% 
+    
+      mutate( r3.loci = r3.b - 1.96 * r3.se, 
+              r3.upci = r3.b + 1.96 * r3.se,
+              r3.or = exp(r3.b), 
+              r3.or_loci = exp(r3.loci), 
+              r3.or_upci = exp(r3.upci),
+              r3.OR_CI = paste0(round(r3.or,2), " [",round(r3.or_loci,2) ,":",round(r3.or_upci,2), "]")) %>% 
+      mutate(r3.effect_direction = ifelse(r3.or_loci > 1 & r3.or_upci >= 1, 'positive',
+                                   ifelse(r3.or_loci < 1 & r3.or_upci <= 1, 'negative', 'overlaps null'))) %>% 
+      
+      select(-contains("loci"), -contains("upci")) %>% 
+    
+      select("exposure.trait", "exposure.id", "outcome.trait", "outcome.id", "med.trait","med.id" , 
+             starts_with("r1"),  starts_with("r2"),starts_with("r3")) %>% 
+      filter(r1.effect_direction != 'overlaps null' &r2.effect_direction != 'overlaps null' & r3.effect_direction != 'overlaps null') %>% 
+      mutate(type = type)
+  } else {
+    df_OR <- data.frame()
+  }
+    
 }
-
-
 
 
 
@@ -104,27 +106,34 @@ res_list_tidy_df <- bind_rows(res_list_tidy)
 res_list_tidy_df %>% select(exposure.trait, med.trait, outcome.trait, r1.b, r2.b, r3.b, type) %>% View()
 
 
-# just get the mediaotors:
-source("explore_MR-EvE_app/functions.R")
-tmp <- res_list_tidy_df %>% #filter(type == 'mediator') %>% 
+
+
+
+# getting the category of the third variable + excluding thing that don't make sense
+source("01_MR_related/explore_MR-EvE_app/functions.R")
+res_list_tidy_df_cats <- res_list_tidy_df %>% #filter(type == 'mediator') %>% 
                   select(exposure.trait = med.trait, 
                          exposure.id = med.id, r3.OR_CI, r3.effect_direction, type) %>% 
-                 create_exposure_categories() %>%
-                 filter(!exposure_cat %in% c('Diseases', 'Medical Procedures', 'other', "Education", "Psychology", "CHD")) 
+                 create_exposure_categories() 
 
-tmp %>% select(type, exposure_cat) %>% group_by(type, exposure_cat) %>% count(type, exposure_cat)
+res_list_tidy_df_cats %>% select(type, exposure_cat) %>% group_by(type, exposure_cat) %>% count(type, exposure_cat)
+
+res_list_tidy_df %>% 
+  select(exposure.trait, med.trait, outcome.trait, r1.OR_CI, r2.OR_CI, r3.OR_CI, r1.b, r2.b, r3.b, type) %>%
+  left_join(res_list_tidy_df_cats %>% select(med.trait = exposure.trait, med_cat = exposure_cat), by='med.trait' ) %>% 
+  select(exposure.trait, med.trait, outcome.trait, r1.OR_CI, r2.OR_CI, r3.OR_CI,  type, med_cat, r1.b, r2.b, r3.b) %>% 
+  distinct() %>% 
+  filter(!med_cat %in% c('Diseases', 'Medical Procedures', 'other', "Education", "Psychology", "CHD")) %>% View() 
 
 
-in_final <- read_tsv("mr_evidence_outputs/trait_manual_ivw_subtypes_merged.tsv") %>% 
+
+in_final <- read_tsv("01_MR_related/mr_evidence_outputs/trait_manual_ivw_subtypes_merged.tsv") %>% 
   filter(exposure_cat %in% c("Proteins", "Metabolites", "Other biomarkers")) %>% 
   filter(!grepl("LDL|HDL|cholest|trigl", exposure_name, ignore.case = T)) %>% 
   pull(id.exposure)
 
-
+# check if the third var was also a trai in the final set
 tmp %>% mutate(also = ifelse(exposure.id %in% in_final, 1, 0)) %>% View()
-
-
-
 
 query = paste0("
     MATCH (gene1:Gene)-[gene_to_protein:GENE_TO_PROTEIN]->(protein:Protein)-[protein_in_pathway:PROTEIN_IN_PATHWAY]->(pathway:Pathway) 
