995129b2172fa36f75346ad12eb3170b2fc55174
Marina Vabistsevits <ny19205@bristol.ac.uk>
2023-06-09 10:20:21 +0100

    add Lit-related changes wip

--- project_code/02_literature_related/scripts/merge_term_to_gwas_tables.R
+++ project_code/02_literature_related/scripts/merge_term_to_gwas_tables.R
@@ -2,28 +2,31 @@
 ## adhoc script for extracting previously mapped lit terms to opengwas  IDs
 
 
-prev_terms1<- read_csv("../results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part1.csv")
-prev_terms2<- read_csv("../results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part2.csv")
-prev_terms3<- read_csv("../results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part3.csv")
-prev_terms4<- read_csv("../results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part4.csv")
-prev_terms5<- read_csv("../results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part5.csv")
-prev_terms6<- read_csv("../results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part6.csv")
-prev_terms7<- read_csv("../results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part7.csv")
+prev_terms1<- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part1.csv")
+prev_terms2<- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part2.csv")
+prev_terms3<- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part3.csv")
+prev_terms4<- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part4.csv")
+prev_terms5<- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part5.csv")
+prev_terms6<- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part6.csv")
+prev_terms7<- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part7.csv")
+prev_terms8<- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part8.csv")
+prev_terms9<- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/terms_to_opengwas_part9.csv")
 
-prev_terms <- bind_rows(prev_terms7,prev_terms6, prev_terms5, prev_terms4, prev_terms3, prev_terms2, prev_terms1) %>% distinct()
 
-new_trait_terms <- read_csv("./results/literature_outputs/sankey_terms_storage/lit_terms_menopause.csv")
+prev_terms <- bind_rows( prev_terms9, prev_terms8, prev_terms7,prev_terms6, prev_terms5, prev_terms4, prev_terms3, prev_terms2, prev_terms1) %>% distinct()
+
+new_trait_terms <- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/lit_terms_calpastatin.csv")
 
 merged<- new_trait_terms %>% left_join(prev_terms) %>% arrange(value)
 
 # coalese rows
 merged <- merged %>%
-  group_by(value) %>%
+  group_by(value, gwas.id) %>%
   fill(everything(), .direction = "downup") %>%
   slice(1)
 
 
-merged %>% write_csv("./results//literature_outputs/sankey_terms_storage/lit_terms_menopause.csv")
+merged %>% write_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/lit_terms_calpastatin.csv")
 
 
 
