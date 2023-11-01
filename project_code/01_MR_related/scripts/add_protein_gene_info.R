6b8ebd1304d1f652ec83875a7e8b0d4a7c54f7ca
Marina Vabistsevits <ny19205@bristol.ac.uk>
2023-11-01 18:26:41 +0000

    getting cis inst for case study intermediates

--- project_code/01_MR_related/scripts/add_protein_gene_info.R
+++ project_code/01_MR_related/scripts/add_protein_gene_info.R
@@ -58,3 +58,79 @@ location_tidy_names <- right_join(location_tidy, protein_names_id, by="gene") %>
 
 write_csv(location_tidy_names, "01_MR_related/results/mr_evidence_outputs/protein_gene_regions_ids.csv")
 
+
+
+#### when working on literature case studies
+#### 
+
+# 1: HDL
+
+# file from USCS http://genome.ucsc.edu/cgi-bin/hgTables 
+# gene IDS pasted from  02_literature_related/results/literature_outputs/sankey_terms_storage/lit_terms_HighDensityLipoproteinsNEW_genes.csv
+
+locations_all <- read_csv("01_MR_related/results/case_study_reports_tidy/literature_proteins_ucsc_data/HDL_ucsc_protein_data.csv") %>% 
+  filter(!grepl("alt|fix|Y|X", hg38.knownGene.chrom)) %>% select(-1) %>% distinct() %>% arrange(hg38.kgXref.geneSymbol)
+
+exls<- read_csv("01_MR_related/results/case_study_reports_tidy/literature_proteins_ucsc_data/HDL_ucsc_protein_data.csv") %>% 
+  filter(grepl("alt|fix|Y|X", hg38.knownGene.chrom)) %>% filter(!hg38.kgXref.geneSymbol %in% locations_all$hg38.kgXref.geneSymbol)
+
+colnames(locations_all) <- c("chrom", 'txStart', 'txEnd', 'name2')
+
+
+# tidy UCSC output
+location_tidy <- tibble()
+for (i in unique(locations_all$name2)){
+  print(i)
+  tmp <- locations_all  %>%  filter(name2 ==  i)
+  start<-min(tmp$txStart)
+  end<-max(tmp$txEnd)
+  chrom<-gsub("chr", "", unique(tmp$chrom))
+  out<-tibble(gene = i, chr=chrom, posStart=start, posEnd=end)
+  #print(out)
+  location_tidy<-bind_rows(location_tidy, out)
+}
+
+protein_names_id <- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/lit_terms_HighDensityLipoproteinsNEW_genes.csv")
+
+location_tidy_names <- left_join(protein_names_id,location_tidy,  by="gene") %>% distinct()
+
+
+write_csv(location_tidy_names, "02_literature_related/results/literature_outputs/sankey_terms_storage/lit_terms_HighDensityLipoproteinsNEW_genes.csv")
+
+
+
+# 1: cBMI
+
+# file from USCS http://genome.ucsc.edu/cgi-bin/hgTables 
+# gene IDS pasted from  02_literature_related/results/literature_outputs/sankey_terms_storage/lit_terms_HighDensityLipoproteinsNEW_genes.csv
+
+locations_all <- read_csv("01_MR_related/results/case_study_reports_tidy/literature_proteins_ucsc_data/cBMI_ucsc_protein_data.csv") %>% 
+  filter(!grepl("alt|fix|Y|X", hg38.knownGene.chrom)) %>% select(-1) %>% distinct() %>% arrange(hg38.kgXref.geneSymbol)
+
+exls<- read_csv("01_MR_related/results/case_study_reports_tidy/literature_proteins_ucsc_data/cBMI_ucsc_protein_data.csv") %>% 
+  filter(grepl("alt|fix|Y|X", hg38.knownGene.chrom)) %>% filter(!hg38.kgXref.geneSymbol %in% locations_all$hg38.kgXref.geneSymbol)
+
+colnames(locations_all) <- c("chrom", 'txStart', 'txEnd', 'name2')
+
+
+# tidy UCSC output
+location_tidy <- tibble()
+for (i in unique(locations_all$name2)){
+  print(i)
+  tmp <- locations_all  %>%  filter(name2 ==  i)
+  start<-min(tmp$txStart)
+  end<-max(tmp$txEnd)
+  chrom<-gsub("chr", "", unique(tmp$chrom))
+  out<-tibble(gene = i, chr=chrom, posStart=start, posEnd=end)
+  #print(out)
+  location_tidy<-bind_rows(location_tidy, out)
+}
+
+protein_names_id <- read_csv("02_literature_related/results/literature_outputs/sankey_terms_storage/lit_terms_childhood_obesity_NEW_genes.csv")
+
+location_tidy_names <- left_join(protein_names_id,location_tidy,  by="gene") %>% distinct()
+
+
+write_csv(location_tidy_names, "02_literature_related/results/literature_outputs/sankey_terms_storage/lit_terms_childhood_obesity_NEW_genes.csv")
+
+
